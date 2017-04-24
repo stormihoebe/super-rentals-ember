fe824f304de6cadaa539169d4b6834dfa3afaa85
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-24 10:13:16 -0700

    add delete and save functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,4 +3,16 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+    
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
